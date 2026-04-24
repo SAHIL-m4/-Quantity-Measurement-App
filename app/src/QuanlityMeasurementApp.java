2c022896cb51ee2314b45062a642b40ed0f938b5
MOHAMMED SAHIL T S <mohammedsahilts@MOHAMMEDs-MacBook-Air-3.local>
2026-04-25 01:00:57 +0530

    use case 7th

--- app/src/QuanlityMeasurementApp.java
+++ app/src/QuanlityMeasurementApp.java
@@ -1,67 +1,53 @@
 import java.util.Objects;
-class Length { 
+class Length {
     private final double value;
     private final LengthUnit unit;
     public enum LengthUnit {
         INCHES(1.0),
         FEET(12.0),
         YARDS(36.0),
-        CENTIMETERS(0.3937); 
-        private final double conversionFactor;
-        LengthUnit(double conversionFactor) {
-            this.conversionFactor = conversionFactor;
-        }
-        public double toBase(double value) {
-            return value * this.conversionFactor;
-        }
-        public double fromBase(double baseValue) {
-            return baseValue / this.conversionFactor;
-        }
+        CENTIMETERS(0.3937);
+        private final double factor;
+        LengthUnit(double factor) { this.factor = factor; }
+        public double toBase(double val) { return val * this.factor; }
+        public double fromBase(double baseVal) { return baseVal / this.factor; }
     }
     public Length(double value, LengthUnit unit) {
-        if (!Double.isFinite(value)) throw new IllegalArgumentException("Value must be finite");
+        if (!Double.isFinite(value)) throw new IllegalArgumentException("Invalid value");
         this.value = value;
-        this.unit = Objects.requireNonNull(unit, "Unit cannot be null");
+        this.unit = Objects.requireNonNull(unit);
     }
-    public Length add(Length thatLength) {
-        Objects.requireNonNull(thatLength, "Operand cannot be null");
-        double totalInBase = this.unit.toBase(this.value) + 
-                             thatLength.unit.toBase(thatLength.value);
-        double convertedResult = this.unit.fromBase(totalInBase);
-        double roundedValue = Math.round(convertedResult * 100.0) / 100.0;
-        return new Length(roundedValue, this.unit);
+    public Length add(Length that) {
+        return add(that, this.unit); 
     }
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Length that = (Length) o;
-        return Math.abs(this.unit.toBase(this.value) - that.unit.toBase(that.value)) < 0.01;
+    public Length add(Length that, LengthUnit targetUnit) {
+        Objects.requireNonNull(that, "Operand length cannot be null");
+        Objects.requireNonNull(targetUnit, "Target unit cannot be null");
+        return addAndConvert(this, that, targetUnit);
+    }
+    private Length addAndConvert(Length l1, Length l2, LengthUnit target) {
+        double sumInBase = l1.unit.toBase(l1.value) + l2.unit.toBase(l2.value);
+        double rawResult = target.fromBase(sumInBase);
+        double rounded = Math.round(rawResult * 100.0) / 100.0;
+        return new Length(rounded, target);
     }
     @Override
     public String toString() {
-        return "Quantity(" + value + ", " + unit + ")";
+        return String.format("Quantity(%s, %s)", value, unit);
     }
 }
 public class QuanlityMeasurementApp {
     public static void main(String[] args) {
-        System.out.println("=== UC6 Length Addition Demonstration ===\n");
-        Length ft1 = new Length(1.0, Length.LengthUnit.FEET);
-        Length ft2 = new Length(2.0, Length.LengthUnit.FEET);
-        printAddition(ft1, ft2);
-        Length in12 = new Length(12.0, Length.LengthUnit.INCHES);
-        printAddition(ft1, in12);
-        printAddition(in12, ft1);
-        Length yd1 = new Length(1.0, Length.LengthUnit.YARDS);
-        Length ft3 = new Length(3.0, Length.LengthUnit.FEET);
-        printAddition(yd1, ft3);
-        Length cmBase = new Length(2.54, Length.LengthUnit.CENTIMETERS);
-        Length in1 = new Length(1.0, Length.LengthUnit.INCHES);
-        printAddition(cmBase, in1);
-    }
-    private static void printAddition(Length l1, Length l2) {
-        Length result = l1.add(l2);
-        System.out.printf("Input:  %s + %s%n", l1, l2);
-        System.out.printf("Output: %s%n%n", result);
+        System.out.println("=== UC7: Addition with Target Unit Specification ===\n");
+        Length oneFoot = new Length(1.0, Length.LengthUnit.FEET);
+        Length twelveInches = new Length(12.0, Length.LengthUnit.INCHES);
+        System.out.println("Target FEET:   " + oneFoot.add(twelveInches, Length.LengthUnit.FEET));
+        System.out.println("Target INCHES: " + oneFoot.add(twelveInches, Length.LengthUnit.INCHES));
+        System.out.println("Target YARDS:  " + oneFoot.add(twelveInches, Length.LengthUnit.YARDS));
+        Length yard1 = new Length(1.0, Length.LengthUnit.YARDS);
+        Length feet3 = new Length(3.0, Length.LengthUnit.FEET);
+        System.out.println("\nCommutativity Check (Target FEET):");
+        System.out.println("A + B: " + yard1.add(feet3, Length.LengthUnit.FEET));
+        System.out.println("B + A: " + feet3.add(yard1, Length.LengthUnit.FEET));
     }
 }
